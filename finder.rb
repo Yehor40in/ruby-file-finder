4b405a25cfbd274db6033e111e4e5f04632e57c4
Yehor <y.sorokin1996@gmail.com>
2020-05-30 16:34:31 +0300

    Add files via upload

---
+++ finder.rb
@@ -0,0 +1,53 @@
+module SF
+
+    class Finder
+
+        @@filesystem = Hash.new
+        @@restricted = ["var", "Photos Library.photoslibrary", "System", ".Trash", "Library", ".fseventsd", "Config.Msi", "System Volume Information", "WindowsApps", ".", ".."]
+
+        def self.init_filesystem_fingerprint(start = "/")
+
+            @@filesystem[start] = Dir.entries(start)
+            loop do
+                temp = @@filesystem.clone
+                temp.each do |path, contents|
+                    contents.each do |entry|
+                        dir = File.join(path, entry)
+                        unless @@filesystem.key? dir then
+                            unless @@restricted.include? entry then
+                                if File.directory?(dir) then
+                                    @@filesystem[dir] = Dir.entries(dir)[2..-1]
+                                end
+                            end
+                        end
+                    end
+                end
+                break if @@filesystem.size > 3000
+            end
+        end
+
+
+        def self.search_for_entries(keyword)
+            
+            matches = []
+            unless @@filesystem.empty? then
+                @@filesystem.each do |path, entries|
+                    matches << path if path.include? keyword
+                end
+                return matches unless matches.empty?
+            end
+            return false
+
+        end
+
+    end
+
+    
+end
+
+SF::Finder.init_filesystem_fingerprint
+if matches = SF::Finder.search_for_entries(ARGV[0]) then
+    puts matches
+else
+    puts "No matches found"
+end

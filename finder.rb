7bd99406205305b813c1cd1851ae7a1e38bd7f02
Yehor <yehorsorokin@icloud.com>
2021-02-04 13:22:12 +0200

    Update finder.rb

--- finder.rb
+++ finder.rb
@@ -1,21 +1,24 @@
-module SF
-
-    class Finder
+class Finder
 
         @@filesystem = Hash.new
+        @@restricted = [".", ".."]
+        @@last_matches = []
 
-        def self.init_filesystem_fingerprint(start = "/")
 
+        def self.init_filesystem_fingerprint(start = "C:/")
             @@filesystem[start] = Dir.entries(start)
+
             loop do
+                did_change = false
                 temp = @@filesystem.clone
                 temp.each do |path, contents|
                     contents.each do |entry|
                         begin
                             dir = File.join(path, entry)
-                            unless @@filesystem.key? dir then
+                            unless @@filesystem.key?(dir) && @@restricted.include?(entry) then
                                 if File.directory?(dir) then
                                     @@filesystem[dir] = Dir.entries(dir)[2..-1]
+                                    did_change = true
                                 end
                             end
                         rescue => exception
@@ -23,32 +26,38 @@ module SF
                         end
                     end
                 end
-                break if @@filesystem.size > 3000 #this is dumb, but good for now
+                break unless did_change
             end
         end
 
 
         def self.search_for_entries(keyword)
-            
-            matches = []
+            @@last_matches = []
             unless @@filesystem.empty? then
                 @@filesystem.each do |path, entries|
-                    matches << path if path.include? keyword
+                    @@last_matches << path if path.include?(keyword) || path.match?(keyword)
                 end
-                return matches unless matches.empty?
+                return !@@last_matches.empty?
             end
-            return false
 
         end
 
+
+        def self.findall(regexps)
+            result = []
+            regexps.each do |regexp| 
+                result.append( self.find regexp ) unless @@last_matches.empty?
+            end
+            return result
+        end
+        
+        def self.find(entry)
+            self.init_filesystem_fingerprint
+            if self.search_for_entries(entry) then
+                return @@last_matches
+            end
+        end
+
+
+
     end
-
-    
-end
-
-SF::Finder.init_filesystem_fingerprint
-if matches = SF::Finder.search_for_entries(ARGV[0]) then
-    puts matches
-else
-    puts "No matches found"
-end
